5915234010ba716ac6e2aee711ac3fddf0864524
Snehil-Kashyap <snehilk31@gmail.com>
2024-03-03 12:52:56 +0530

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,9 +8,7 @@ const stripe = Stripe(
 export const bookTour = async (tourID) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourID}`,
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourID}`);
     // console.log(session);
 
     // 2) Create checkout form + charge credit card
