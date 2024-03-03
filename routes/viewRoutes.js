d2dd4479875bfe7b5166e055a70005d7071921cb
Snehil-Kashyap <snehilk31@gmail.com>
2024-03-03 15:06:50 +0530

    Changes done

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get(
   '/',
-  bookingControllers.createBookingCheckout,
+  // bookingControllers.createBookingCheckout,
   authControllers.isLoggedIn,
   viewsController.getOverview,
 );
